49d818c8774ed1dede6a932ae7eafb83e8ef2972
Earl_Groenewald <earl.groenewald@standardbank.co.za>
2019-11-14 23:01:00 +0200

    Third commit, gettin the hang of this now. Added 6th id

--- SimpleServer.js
+++ SimpleServer.js
@@ -11,7 +11,9 @@ const players = [
     {id: 2 , name: 'Ronaldo', level: 'Okay'},
     {id: 3 , name: 'James', level: 'Who is this guy?'},
     {id: 4 , name: 'An Earl', level: 'Majestic, a beauty to watch. Will bring you to tears'},
-    {id: 5 , name: 'Lee', level: 'If you liked Earl, you\'ll love Lee. The greatest of his time'}
+    {id: 5 , name: 'Lee', level: 'If you liked Earl, you\'ll love Lee. The greatest of his time'},
+    {id: 6 , name: 'Christian', level: 'Some say he\'s the future of football. We think they\'re wrong. He\'s... The future'}
+
 ];
 
 
