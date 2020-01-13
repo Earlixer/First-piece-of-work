045176ccdc8c914ec3d58f152b27247870084a03
Earl_Groenewald <earl.groenewald@standardbank.co.za>
2020-01-13 22:38:14 +0200

    Random commit number 3

--- SimpleServer.js
+++ SimpleServer.js
@@ -13,7 +13,8 @@ const players = [
     {id: 4 , name: 'An Earl', level: 'Majestic, a beauty to watch. Will bring you to tears'},
     {id: 5 , name: 'Lee', level: 'If you liked Earl, you\'ll love Lee. The greatest of his time'},
     {id: 6 , name: 'Christian', level: 'Some say he\'s the future of football. We think they\'re wrong. He\'s... The future'},
-    {id: 7 , name: 'Learning Branches', level: 'This is after I switched to Second-Branch'}
+    {id: 7 , name: 'Learning Branches', level: 'This is after I switched to Second-Branch'},
+    {id: 8 , name: 'Nela', level: 'My girlie :)'}
 ];
 
 
